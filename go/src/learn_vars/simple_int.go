ccc97e9b487f299b02466c9bbc4564f20cc5f4b4
GPanula <github@kablah.com>
2018-02-28 19:23:13 -0600

    more multi-variable fun

--- go/src/learn_vars/simple_int.go
+++ go/src/learn_vars/simple_int.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var width, height = 100, 50 // declare two vars in a single go
+	var width, height int // declare two vars in a single go
+	fmt.Println("width is", width, "height is", height)
+	// now assign values to width and height
+	width = 75
+	height = 200
 	fmt.Println("width is", width, "height is", height)
 }
