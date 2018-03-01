624ff7a728540bc1e1a2fdca9dc939770cb68bb0
GPanula <github@kablah.com>
2018-02-28 19:07:35 -0600

    assigned some values to age

--- go/src/learn_vars/simple_int.go
+++ go/src/learn_vars/simple_int.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	var age int // declare age variable as an integer
 	fmt.Println("declared age int and its value is ", age)
+	age = 29 // assign value of 29 to age
+	fmt.Println("age now has value of", age)
+	age = 54 // update value of age
+	fmt.Println("new value for age is", age)
 }
