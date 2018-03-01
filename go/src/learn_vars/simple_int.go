3442c1dd5726a1ac16c79a05d48868fed155ad25
GPanula <github@kablah.com>
2018-02-28 19:19:55 -0600

    remove explicit variable declaration

--- go/src/learn_vars/simple_int.go
+++ go/src/learn_vars/simple_int.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var width, height int = 100, 50 // declare two vars in a single go
+	var width, height = 100, 50 // declare two vars in a single go
 	fmt.Println("width is", width, "height is", height)
 }
