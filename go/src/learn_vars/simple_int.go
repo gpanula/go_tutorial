93472a7bafb073c727fa5f879630c5f46af516d5
GPanula <github@kablah.com>
2018-02-28 19:32:37 -0600

    another way of declaring multiple variables

--- go/src/learn_vars/simple_int.go
+++ go/src/learn_vars/simple_int.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		name   = "sugar monkey"
-		age    = 34
-		height int
-	)
+	// declare our initial variables with their initial values
+	name, age, height := "sugar monster", 34, 2
+
 	fmt.Println("The", name, "is", age, " years old and", height, " meters tall")
 }
